d55363d2ff31962f861723ffde662c070d8278f7
JPeeh <131622056+JPeeh@users.noreply.github.com>
2023-08-22 09:51:35 -0300

    Add files via upload

---
+++ Atividade1.java
@@ -0,0 +1,28 @@
+public class Atividade1 {
+
+    public static int verificaNumeroPrimo(Integer n) {
+        if (n <= 1) {
+            return 0; 
+        }
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return 0; 
+            }
+        }
+
+        return 1; 
+    }
+
+    public static void main(String[] args) {
+        Integer numero = 1; // Número que deseja verificar
+
+        int resultado = verificaNumeroPrimo(numero);
+
+        if (resultado == 1) {
+            System.out.println(numero + " é um número primo.");
+        } else {
+            System.out.println(numero + " não é um número primo.");
+        }
+    }
+}
