3275dee2d18399e3615c0ffd07d6aab42bb0d865
joao pedro holanda de souza <131622056+JPeeh@users.noreply.github.com>
2023-08-22 10:14:33 -0300

    Update exercicio1.java

--- atividade2/exercicio1.java
+++ atividade2/exercicio1.java
@@ -1,1 +1,45 @@
+import java.util.Scanner;
+
+public class exercicio1 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro número: ");
+        double numero1 = scanner.nextDouble();
+
+        System.out.print("Digite o segundo número: ");
+        double numero2 = scanner.nextDouble();
+
+        System.out.print("Escolha a operação (+, -, *, /): ");
+        char operacao = scanner.next().charAt(0);
+
+        double resultado = 0;
+
+        switch (operacao) {
+            case '+':
+                resultado = numero1 + numero2;
+                break;
+            case '-':
+                resultado = numero1 - numero2;
+                break;
+            case '*':
+                resultado = numero1 * numero2;
+                break;
+            case '/':
+                if (numero2 != 0) {
+                    resultado = numero1 / numero2;
+                } else {
+                    System.out.println("Erro: Divisão por zero não é permitida.");
+                    System.exit(1);
+                }
+                break;
+            default:
+                System.out.println("Operação inválida.");
+                System.exit(1);
+        }
+
+        System.out.println("Resultado: " + resultado);
+    }
+}
 
